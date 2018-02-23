716a4e90e8d315a0c4b81bfa262f9ee8cf288812
kamulau <kamuela.lau@gmail.com>
2018-02-23 10:34:24 +0900

    Added AL2 license headers to java source files. This closes #4

--- src/main/java/org/ltr4l/tools/Report.java
+++ src/main/java/org/ltr4l/tools/Report.java
@@ -1,5 +1,5 @@
 /*
- * Copyright [yyyy] [name of copyright owner]
+ * Copyright 2018 org.LTR4L
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
