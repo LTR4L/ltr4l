3996733555ed3ff71f4e2324c013317d2d6c15b0
koji <koji.sekiguchi@rondhuit.com>
2018-03-21 17:43:28 +0900

    add convenient methods for MLP tests in order to simplify them

--- src/test/java/org/ltr4l/nn/MLPTest.java
+++ src/test/java/org/ltr4l/nn/MLPTest.java
@@ -50,19 +50,24 @@ public class MLPTest {
 
     MLP.Edge outputEdge = inputNode.getOutputEdge(0);
     MLP.Edge inputEdge0 = hiddenNode.getInputEdge(0);
-    Assert.assertFalse(outputEdge == inputEdge0);
-    MLP.Edge inputEdge1 = hiddenNode.getInputEdge(1);
-    Assert.assertTrue(outputEdge == inputEdge1);
-
-    Assert.assertTrue(outputEdge.getSource() == inputNode);
-    Assert.assertTrue(outputEdge.getDestination() == hiddenNode);
-    Assert.assertNull(inputEdge0.getSource());
-    Assert.assertTrue(inputEdge0.getDestination() == hiddenNode);
-    Assert.assertTrue(inputEdge1.getSource() == inputNode);
-    Assert.assertTrue(inputEdge1.getDestination() == hiddenNode);
+    assertBetweenNodes(inputNode, 0, hiddenNode, 1);
+    assertBiasEdge(inputEdge0, hiddenNode);
 
     Assert.assertTrue(outputEdge.getOptimizer() instanceof Optimizer.SGD);
     Assert.assertTrue(inputEdge0.getOptimizer() instanceof Optimizer.SGD);
+  }
+
+  private void assertBetweenNodes(MLP.MNode sn, int i, MLP.MNode dn, int j) throws Exception {
+    MLP.Edge oe = sn.getOutputEdge(i);
+    MLP.Edge ie = dn.getInputEdge(j);
+    Assert.assertTrue(oe == ie);
+    Assert.assertTrue(sn == oe.getSource());
+    Assert.assertTrue(dn == oe.getDestination());
+  }
+
+  private void assertBiasEdge(MLP.Edge edge, MLP.MNode dn) throws Exception {
+    Assert.assertNull(edge.getSource());
+    Assert.assertTrue(dn == edge.getDestination());
   }
 
   @Test
@@ -101,32 +106,18 @@ public class MLPTest {
     Assert.assertEquals(0, hiddenNode0.getOutputEdges().size());
     Assert.assertEquals(0, hiddenNode1.getOutputEdges().size());
 
-    MLP.Edge outputEdge00 = inputNode0.getOutputEdge(0);
-    MLP.Edge outputEdge01 = inputNode0.getOutputEdge(1);
-    MLP.Edge outputEdge10 = inputNode1.getOutputEdge(0);
-    MLP.Edge outputEdge11 = inputNode1.getOutputEdge(1);
     MLP.Edge inputEdge00 = hiddenNode0.getInputEdge(0);
     MLP.Edge inputEdge01 = hiddenNode0.getInputEdge(1);
     MLP.Edge inputEdge02 = hiddenNode0.getInputEdge(2);
     MLP.Edge inputEdge10 = hiddenNode1.getInputEdge(0);
     MLP.Edge inputEdge11 = hiddenNode1.getInputEdge(1);
     MLP.Edge inputEdge12 = hiddenNode1.getInputEdge(2);
-    Assert.assertTrue(outputEdge00 == inputEdge01);
-    Assert.assertTrue(outputEdge01 == inputEdge11);
-    Assert.assertTrue(outputEdge10 == inputEdge02);
-    Assert.assertTrue(outputEdge11 == inputEdge12);
-
-    Assert.assertTrue(outputEdge00.getSource() == inputNode0);
-    Assert.assertTrue(outputEdge00.getDestination() == hiddenNode0);
-    Assert.assertTrue(outputEdge01.getSource() == inputNode0);
-    Assert.assertTrue(outputEdge01.getDestination() == hiddenNode1);
-    Assert.assertTrue(outputEdge10.getSource() == inputNode1);
-    Assert.assertTrue(outputEdge10.getDestination() == hiddenNode0);
-    Assert.assertTrue(outputEdge11.getSource() == inputNode1);
-    Assert.assertTrue(outputEdge11.getDestination() == hiddenNode1);
-
-    Assert.assertNull(inputEdge00.getSource());
-    Assert.assertNull(inputEdge10.getSource());
+    assertBetweenNodes(inputNode0, 0, hiddenNode0, 1);
+    assertBetweenNodes(inputNode0, 1, hiddenNode1, 1);
+    assertBetweenNodes(inputNode1, 0, hiddenNode0, 2);
+    assertBetweenNodes(inputNode1, 1, hiddenNode1, 2);
+    assertBiasEdge(inputEdge00, hiddenNode0);
+    assertBiasEdge(inputEdge10, hiddenNode1);
 
     Assert.assertTrue(inputEdge00.getOptimizer() instanceof Optimizer.SGD);
     Assert.assertTrue(inputEdge01.getOptimizer() instanceof Optimizer.SGD);
@@ -167,25 +158,14 @@ public class MLPTest {
     Assert.assertEquals(2, hiddenNode1.getInputEdges().size());
     Assert.assertEquals(0, hiddenNode1.getOutputEdges().size());
 
-    MLP.Edge outputEdge0 = inputNode.getOutputEdge(0);
-    MLP.Edge outputEdge1 = hiddenNode0.getOutputEdge(0);
     MLP.Edge inputEdge00 = hiddenNode0.getInputEdge(0);
     MLP.Edge inputEdge01 = hiddenNode0.getInputEdge(1);
     MLP.Edge inputEdge10 = hiddenNode1.getInputEdge(0);
     MLP.Edge inputEdge11 = hiddenNode1.getInputEdge(1);
-    Assert.assertTrue(outputEdge0 == inputEdge01);
-    Assert.assertTrue(outputEdge1 == inputEdge11);
-
-    Assert.assertTrue(outputEdge0.getSource() == inputNode);
-    Assert.assertTrue(outputEdge0.getDestination() == hiddenNode0);
-    Assert.assertNull(inputEdge00.getSource());
-    Assert.assertTrue(inputEdge00.getDestination() == hiddenNode0);
-    Assert.assertTrue(inputEdge01.getSource() == inputNode);
-    Assert.assertTrue(inputEdge01.getDestination() == hiddenNode0);
-    Assert.assertNull(inputEdge10.getSource());
-    Assert.assertTrue(inputEdge10.getDestination() == hiddenNode1);
-    Assert.assertTrue(inputEdge11.getSource() == hiddenNode0);
-    Assert.assertTrue(inputEdge11.getDestination() == hiddenNode1);
+    assertBetweenNodes(inputNode, 0, hiddenNode0, 1);
+    assertBetweenNodes(hiddenNode0, 0, hiddenNode1, 1);
+    assertBiasEdge(inputEdge00, hiddenNode0);
+    assertBiasEdge(inputEdge10, hiddenNode1);
 
     Assert.assertTrue(inputEdge00.getOptimizer() instanceof Optimizer.SGD);
     Assert.assertTrue(inputEdge01.getOptimizer() instanceof Optimizer.SGD);
