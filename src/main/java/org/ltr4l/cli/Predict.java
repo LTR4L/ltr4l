b4170ef62a71f7c1f78a1cbe18ace0a770dfd64c
kamulau <kamuela.lau@gmail.com>
2018-04-03 10:23:12 +0900

    Added Predict class.

---
+++ src/main/java/org/ltr4l/cli/Predict.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2018 org.LTR4L
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ltr4l.cli;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.cli.*;
+import org.ltr4l.Ranker;
+import org.ltr4l.Version;
+import org.ltr4l.nn.MLP;
+import org.ltr4l.nn.RankNetMLP;
+import org.ltr4l.query.Document;
+import org.ltr4l.query.QuerySet;
+import org.ltr4l.tools.Config;
+import org.ltr4l.trainers.MLPTrainer;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Predict {
+
+  private static final String REQUIRED_ARG = "<LTR-algorithm-name>";
+
+  public static void main (String args[]) throws Exception{
+    Options options = createOptions();
+    CommandLine line = getCommandLine(options, args);
+
+    if(line.hasOption("help")) printUsage(options);
+
+    if(line.hasOption("version")){
+      System.out.printf("%s release %s\n", MethodHandles.lookup().lookupClass().getCanonicalName(), Version.version);
+      System.exit(0);
+    }
+
+    // get LTR-algorithm-name
+    String[] params = line.getArgs();
+    if(params == null || params.length == 0){
+      System.err.printf("No required argument %s specified", REQUIRED_ARG);
+      printUsage(options);
+    }
+    else if(params.length > 1){
+      System.err.printf("Too many argument is specified: %s", params[1]);
+      printUsage(options);
+    }
+
+    String configPath = getModelPath(line, params);
+    Config optionalConfig = createOptionalConfig(configPath, line);
+
+    QuerySet testSet = QuerySet.create(optionalConfig.dataSet.test);
+
+
+
+  }
+
+  public static Options createOptions(){
+    Option help = new Option( "help", "print this message" );
+    Option modelFile = Option.builder("model").argName("file").hasArg()
+        .desc("use given file for configuration and model").build();
+    Option testDataSet = Option.builder("test").argName("file").hasArg()
+        .desc("use given file for testing the model").build();
+    Option reportFile = Option.builder("report").argName("file").hasArg()
+        .desc("specify report file name").build();
+    Option version = new Option( "version", "print the version information and exit" );
+    Option verbose = new Option( "verbose", "be extra verbose" );
+    Option debug = new Option( "debug", "print debugging information" );
+
+    Options options = new Options();
+    options.addOption(help)
+        .addOption(modelFile)
+        .addOption(testDataSet)
+        .addOption(reportFile)
+        .addOption(version)
+        .addOption(verbose)
+        .addOption(debug);
+    return options;
+  }
+
+  public static void printUsage(Options options){
+    HelpFormatter formatter = new HelpFormatter();
+    formatter.printHelp( "predict " + REQUIRED_ARG,
+        "\nExecute Learning-to-Rank predicting algorithm. The algorithm is specified by the required argument <LTR-algorithm-name>." +
+            " The program will look for the model file \"model/<LTR-algorithm-name>-model.json\"" +
+            " unless model option is specified." +
+            " The following options can be specified in order to override the existing settings in the config file.\n\n",
+        options, null, true );
+    System.exit(0);
+  }
+
+  public static CommandLine getCommandLine(Options options, String[] args){
+    CommandLineParser parser = new DefaultParser();
+    CommandLine line = null;
+    try {
+      // parse the command line arguments
+      line = parser.parse(options, args);
+    }
+    catch(ParseException exp) {
+      // oops, something went wrong
+      System.err.printf("Parsing failed. Reason: %s\n\n", exp.getMessage());
+      printUsage(options);
+    }
+
+    return line;
+  }
+
+  public static String getModelPath(CommandLine line, String[] params){
+    assert(params.length == 1);
+    return line.hasOption("model") ? line.getOptionValue("model") : String.format("model/%s-model.json", params[0]);
+  }
+
+  public static Config createOptionalConfig(String configPath, CommandLine line) throws IOException{
+    ObjectMapper mapper = new ObjectMapper();
+    Config optionalConfig = mapper.readValue(new File(configPath), Config.class);
+
+    if(line.hasOption("test"))
+      optionalConfig.dataSet.test = line.getOptionValue("training");
+    if(line.hasOption("report"))
+      optionalConfig.report.file = line.getOptionValue("report");
+
+    return optionalConfig;
+  }
+
+}
