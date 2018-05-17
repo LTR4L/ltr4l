f94a64c45dfa7a52b61643c68fb4c3c4faba6b30
kamulau <kamuela.lau@gmail.com>
2018-05-17 13:05:10 +0900

    Added training tests for LambdaMartTrainer. This closes #206, #204, and #203

--- src/main/java/org/ltr4l/trainers/LambdaMartTrainer.java
+++ src/main/java/org/ltr4l/trainers/LambdaMartTrainer.java
@@ -132,7 +132,7 @@ public class LambdaMartTrainer extends AbstractTrainer<Ensemble, Ensemble.TreeCo
     for (int t = 1; t <= numTrees; t++){
       int minLossFeat = findMinLossFeat(thresholds, minLoss);
       if(minLossFeat == -1){
-        System.out.println("Could not find valid feature with specified minimum loss. Stopping training early.");
+        System.out.printf("Could not find valid feature with specified minimum loss. Stopping training early at tree %d \n", t);
         return;
       }
       //First, calculate lambdas for this iteration.
