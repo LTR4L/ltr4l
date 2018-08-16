0dd3c6be5f5764fcbd6b31c352e0f7cdd2e1a5c6
yasufumi0410 <yasufumi0410@gmail.com>
2018-08-17 04:06:49 +0900

    prepare the next release 0.2

--- src/test/java/org/ltr4l/VersionTest.java
+++ src/test/java/org/ltr4l/VersionTest.java
@@ -23,6 +23,6 @@ public class VersionTest {
 
   @Test
   public void test() throws Exception {
-    Assert.assertEquals("0.1.0", Version.version);
+    Assert.assertEquals("0.2-dev", Version.version);
   }
 }
